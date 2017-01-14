ad848d54a04dc781e389a5f13d0604d2b190a748
DaniLomba <danigt.lomba2@gmail.com>
2017-01-14 23:28:16 +0000

    TEst

--- Placard/Placard.cpp
+++ Placard/Placard.cpp
@@ -2,17 +2,87 @@
 //
 
 #include "stdafx.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct sAposta
+{
+	Cliente umCliente;
+	int valorAposta;
+	char jogo[100];
+}Aposta;
+
+typedef struct sModalidade
+{
+	char modalidade[30];
+}Modalidade;
+typedef struct sModalidades
+{
+	Modalidade modalidades[10];
+}Modalidades;
+
+typedef struct sCliente
+{
+	char nome[100];
+	float saldo;
+	int nrApostas[100];
+}Cliente;
+typedef struct sCLientes
+{
+	Cliente clientes[1000];
+}Clientes;
+
+void Carregamento(Cliente a, float carregamento)
+{
+	a.saldo += carregamento;
+}
 
 
-//joao Ricardo
+void SeparaParaArray(char* arrayMod[], char modalidades[], char carater[])
+{
 
+	int index = 0;
 
-//teste2
+	char *modalidade = strtok(modalidades, carater);
 
+	// Enquando exister o separador.
+	while (modalidade != NULL)
+	{
+		arrayMod[index] = modalidade;
+		index++;
+
+		modalidade = strtok(NULL, carater);
+	}
+
+	// Será necessário que todos as posições do array não preenchidas seja NULL, para mais tarde usar como condição de paragem.
+	for (int i = index; i < 10; i++)
+	{
+		arrayMod[i] = NULL;
+	}
+}
+
+char* ImportaModalidades(char path[])
+{
+	char str[49];
+
+	FILE *fp = fopen(path, "r");
+	fgets(str, 49, fp);
+
+	fclose(fp);
+
+	return str;
+}
 
 int main()
 {
-	printf("Oie");
-		return 0;
-}
+	char str[255];
+	strcpy(str, ImportaModalidades("C:\\Users\\Rafael\\Documents\\Visual Studio 2015\\Projects\\AED_TrabalhoPratico\\Debug\\mod.txt"));
 
+	char *arrayModalidades[10];
+	SeparaParaArray(arrayModalidades, str, ";");
+
+	// Teste Pull GIT
+
+	return 0;
+}
